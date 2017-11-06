4345a1d8e18753f312b85930947dcdd42c1d5228
github username <github email address>
2017-11-06 17:40:02 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,5 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
@@ -9,9 +9,21 @@ class StudentsController < ApplicationController
   end
 
   def new
+    @student = Student.new
   end
 
+  # def create
+  #   Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+  #   redirect_to students_path(@student)
+	# end
+
   def create
+    @student = Student.new
+    @student.first_name = params[:first_name]
+    @student.last_name = params[:last_name]
+    # byebug
+    @student.save
+    redirect_to student_path(@student)
   end
 
 end
